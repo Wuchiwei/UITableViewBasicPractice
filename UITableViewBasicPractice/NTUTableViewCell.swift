ab7897d6fe658b6c2f38a82748d7120964320405
Luke <wuchiwei17@gmail.com>
2019-07-26 21:07:36 +0800

    Step 3: 1. Add a new swift file. 2. Choose Cocoa Touch Class. Name a class with name "NTUTableViewCell" and it inherits from UITableViewCell. 3. Make cell on the storyboard mapping with NTUTableViewCell Class 4. Connect a @IBOutlet with label named "NTULabel".

---
+++ UITableViewBasicPractice/NTUTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  NTUTableViewCell.swift
+//  UITableViewBasicPractice
+//
+//  Created by WU CHIH WEI on 2019/7/26.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class NTUTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ntuLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
