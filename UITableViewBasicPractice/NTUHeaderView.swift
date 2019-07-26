836cb9ed4ce9dbdf2279efa38268f65b385cf2ac
Luke <wuchiwei17@gmail.com>
2019-07-26 21:47:24 +0800

    Step 9: 1. Add NTUHeaderView both swift file and xib.

---
+++ UITableViewBasicPractice/NTUHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  NTUHeaderView.swift
+//  UITableViewBasicPractice
+//
+//  Created by WU CHIH WEI on 2019/7/26.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class NTUHeaderView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var headerLabel: UILabel!
+
+}
