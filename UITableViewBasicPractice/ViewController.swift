baf7daa8827be4d22a0639ba41b6c6e61c85a88a
Luke <wuchiwei17@gmail.com>
2019-07-26 21:16:45 +0800

    Step 5: Save view controller instance into tableView's dataSource property.
    tableView.dataSource = self

--- UITableViewBasicPractice/ViewController.swift
+++ UITableViewBasicPractice/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
 
     //MARK: - UITableViewDataSource
