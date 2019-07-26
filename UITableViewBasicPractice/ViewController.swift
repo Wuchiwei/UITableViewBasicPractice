3ab9d6e5273da51e18b697252510dbfafbbecd53
Luke <wuchiwei17@gmail.com>
2019-07-26 20:58:34 +0800

    Initial Commit

---
+++ UITableViewBasicPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewBasicPractice
+//
+//  Created by WU CHIH WEI on 2019/7/26.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
