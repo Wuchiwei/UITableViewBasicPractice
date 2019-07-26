420bef88f9e51573901228aa45650318910a7667
Luke <wuchiwei17@gmail.com>
2019-07-26 22:07:58 +0800

    Step 14: 1. Add NTUFooterView swift and xib. 2. Connect footLabel.

---
+++ UITableViewBasicPractice/NTUFooterView/NTUFooterView.swift
@@ -0,0 +1,15 @@
+//
+//  NTUFooterView.swift
+//  UITableViewBasicPractice
+//
+//  Created by WU CHIH WEI on 2019/7/26.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class NTUFooterView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var footerLabel: UILabel!
+
+}
